07976ce29e2654a8e718173d7986029609dc1f78
Ellie Fagerberg <hi@ellie.moe>
2023-04-23 00:13:30 +0200

    feat: support projects using src folder

--- install.js
+++ install.js
@@ -4,19 +4,25 @@ const copyRecursiveSync = require("./utilities/copyRecursiveSync");
 const copyFile = require("./utilities/copyFile");
 
 module.exports = () => {
+  const useSrc =
+    fs.existsSync(path.resolve(process.cwd()), "./src/app") ||
+    fs.existsSync(path.resolve(process.cwd()), "./src/pages");
+
+  const basePath = useSrc ? "./src" : ".";
+
   // Copy handlers into /api
   copyRecursiveSync(
     path.resolve(__dirname, "./templates/pages/api"),
-    path.resolve(process.cwd(), "./pages/api")
+    path.resolve(process.cwd(), `${basePath}/pages/api`)
   );
 
   // Copy admin into /app
   copyRecursiveSync(
     path.resolve(__dirname, "./templates/app"),
-    path.resolve(process.cwd(), "./app")
+    path.resolve(process.cwd(), `${basePath}/app`)
   );
 
-  const payloadConfigPath = path.resolve(process.cwd(), "./payload");
+  const payloadConfigPath = path.resolve(process.cwd(), `${basePath}/payload`);
 
   if (!fs.existsSync(payloadConfigPath)) {
     fs.mkdirSync(payloadConfigPath);
@@ -25,13 +31,13 @@ module.exports = () => {
   // Copy payload initialization
   copyFile(
     path.resolve(__dirname, "./templates/payloadClient.ts"),
-    path.resolve(process.cwd(), "./payload/payloadClient.ts")
+    path.resolve(process.cwd(), `${basePath}/payload/payloadClient.ts`)
   );
 
   // Copy base payload config
   copyFile(
     path.resolve(__dirname, "./templates/payload.config.ts"),
-    path.resolve(process.cwd(), "./payload/payload.config.ts")
+    path.resolve(process.cwd(), `${basePath}/payload/payload.config.ts`)
   );
 
   process.exit(0);
