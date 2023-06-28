e38ab1b4dee6a4aa3d62850af6ec8f506445ee71
Jacob Fletcher <jacobsfletch@gmail.com>
2023-06-28 10:36:10 -0400

    chore: removes serverComponentsExternalPackages from next config

--- withPayloadPlugin.js
+++ withPayloadPlugin.js
@@ -45,11 +45,6 @@ const withPayload = async (config, paths) => {
       ...config.experimental,
       appDir: true,
       outputFileTracingExcludes,
-      serverComponentsExternalPackages: [
-        ...(config?.experimental?.serverComponentsExternalPackages || []),
-        "mongoose",
-        "payload",
-      ],
     },
     webpack: (webpackConfig, webpackOptions) => {
       const incomingWebpackConfig =
