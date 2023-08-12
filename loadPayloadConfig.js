fec2ed0f05f177750340cb16f0e027e16931855f
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2023-08-12 02:23:29 +0200

    fix: fixed failed to handle: base_dir() must be absolute
    Error: failed to handle: base_dir() must be absolute. Please ensure that jsc.baseUrl is specified correctly.

--- loadPayloadConfig.js
+++ loadPayloadConfig.js
@@ -1,43 +1,46 @@
-const swcRegister = require('@swc/register');
-const { getTsconfig } = require('get-tsconfig');
-const { clientFiles } = require('payload/dist/config/clientFiles');
+const path = require("path");
+const swcRegister = require("@swc/register");
+const { getTsconfig } = require("get-tsconfig");
+const { clientFiles } = require("payload/dist/config/clientFiles");
 
 const tsConfig = getTsconfig();
 
 const loadPayloadConfig = async (configPath) => {
   const swcOptions = {
-    sourceMaps: 'inline',
+    sourceMaps: "inline",
     jsc: {
       parser: {
-        syntax: 'typescript',
+        syntax: "typescript",
         tsx: true,
       },
       paths: undefined,
       baseUrl: undefined,
     },
     module: {
-      type: 'commonjs',
+      type: "commonjs",
     },
   };
 
   clientFiles.forEach((ext) => {
     require.extensions[ext] = () => null;
   });
-  
+
   if (tsConfig?.config?.compilerOptions?.paths) {
     swcOptions.jsc.paths = tsConfig?.config?.compilerOptions?.paths;
-  
+
     if (tsConfig?.config?.compilerOptions?.baseUrl) {
-      swcOptions.jsc.baseUrl = tsConfig?.config?.compilerOptions?.baseUrl;
+      swcOptions.jsc.baseUrl = path.resolve(
+        tsConfig?.config?.compilerOptions?.baseUrl
+      );
     }
   }
-  
-  swcRegister(swcOptions)
-  
+
+  swcRegister(swcOptions);
+
   const configPromise = require(configPath);
   let config = await configPromise;
   if (config.default) config = await config.default;
   return config;
-}
+};
 
 module.exports = loadPayloadConfig;
