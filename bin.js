0d772f354eb987e28675002ba4a637b68a8a6c30
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-11-27 16:20:48 -0500

    chore: cleanup

--- bin.js
+++ bin.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
-const minimist = require('minimist');
+const minimist = require("minimist");
 
 const args = minimist(process.argv.slice(2));
-const scriptIndex = args._.findIndex((x) => x === 'install');
+const scriptIndex = args._.findIndex((x) => x === "install");
 const script = scriptIndex === -1 ? args._[0] : args._[scriptIndex];
 
 switch (script.toLowerCase()) {
-  case 'install': {
-    require('./install')();
-    break;
-  }
-  
-  case 'build': {
-    require('./build')();
+  case "install": {
+    require("./install")();
     break;
   }
 
